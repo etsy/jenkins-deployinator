57775092ead96cf2bf64b3491ebb52bf9e4f0bc4
llincoln <llincoln@fd1de9fb-0608-4dbb-a15e-3e05b1fa12c1>
2011-05-11 16:10:41 +0000

    AUTO-519  Need to specify repository for maven dependencies in pom.xml

--- src/main/java/com/etsy/jenkins/cli/DeployinatorCommand.java
+++ src/main/java/com/etsy/jenkins/cli/DeployinatorCommand.java
@@ -259,7 +259,7 @@ public class DeployinatorCommand extends CLICommand {
     public String getDeployinatorDiffUrl() {
       // TODO Add a global config var for deployinator host
       return String.format(
-          "http://deployinator.etsycorp.com/diff/%s/%s/%s",
+          "http://deployinator.etsycorp.com/diff/%s/%s/%s/compare",
           this.deployType,
           this.oldRevision,
           this.newRevision);
